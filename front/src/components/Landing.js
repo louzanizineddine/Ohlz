59fff9e2a4aa4bdf85ab1cf94e66cdacb8862b6f
Ouassim-Hamdani <o_hamdani@estin.dz>
2024-03-09 20:09:44 +0100

    Started front setup up react & structure

---
+++ front/src/components/Landing.js
@@ -0,0 +1,8 @@
+
+export const Landing = () => {
+  return (
+    <div className="h-screen w-screen bg-[#D4F0F1] font-poppins flex flex-col items-center justify-center">
+        <h1 className="text-gray-800 text-6xl font-bold">Let's Travel <span className="text-[#DC6F32]">Lille</span>!</h1>
+    </div>
+  )
+}
